8131489f13d29ad41899c6079c08fce227b70b38
Ishan Desai <idesai1210@gmail.com>
2018-08-06 23:30:25 -0500

    Story 2 First Iteration

--- story1.py
+++ story1.py
@@ -18,7 +18,9 @@ def listOfAllAppByDepartments(d):
 def noOfCPUMemByDep(d):
 
     group = d.groupby('Group')
+    #print(group)
     cpu_json = group['CPU cores'].agg([np.sum]).to_json(orient='index')
+    #print(group['CPU cores'].agg([np.sum]))
     memory = group['RAM (MB)'].agg([np.sum]).to_json(orient='index')
     return [("CPU", cpu_json), ("Memory", memory)]
 
