9968717e7aac3dadb8b5eec639b9324b8b4a7252
Ishan Desai <idesai1210@gmail.com>
2018-08-07 16:31:44 -0500

    Story 2 - Cost of Migration by Department

--- story2.py
+++ story2.py
@@ -5,34 +5,16 @@ import numpy as np
 
 def Estimate(d):
 
-    group = d.groupby('Group')
-    # print(group)
-    cpu_json = group['Price'].agg([np.sum]).to_json(orient='index')
-    #print(group['Price'].agg([np.sum]))
-
-    return group['Price'].agg([np.sum])
-
-
-def main():
-    # Assign spreadsheet filename to `file`
-    file = 'hardware.xlsx'
-
-    # Load spreadsheet
-    xl = pd.ExcelFile(file)
-
-    # Print the sheet names
-    #print(xl.sheet_names)
-
     # Load a sheet into a DataFrame by name: df1
-    df1 = xl.parse('Page 1')
+    df1 = d
 
     file = 'prices.xlsx'
     xl1 = pd.ExcelFile(file)
-    #print(xl1.sheet_names)
+    # print(xl1.sheet_names)
     df2 = xl1.parse('Sheet1')
 
-    #print(df1)
-    #print(df2)
+    # print(df1)
+    # print(df2)
 
     my_df = {}
     my_df['Type'] = list()
@@ -40,45 +22,55 @@ def main():
     my_df['Price'] = list()
 
     for index, row in df1.iterrows():
-        #print(row["CPU cores"], row["RAM (MB)"])
-        #print(df2.loc[(df2['CPU'] <= row["CPU cores"]) | (df2["RAM (MB)"] <= row["RAM (MB)"])].iloc[-1])
+        # print(row["CPU cores"], row["RAM (MB)"])
+        # print(df2.loc[(df2['CPU'] <= row["CPU cores"]) | (df2["RAM (MB)"] <= row["RAM (MB)"])].iloc[-1])
         l = df2.loc[(df2['CPU'] <= row["CPU cores"]) | (df2["RAM (MB)"] <= row["RAM (MB)"])].iloc[-1]
         # print(l['Type'])
         my_df['Type'].append(l['Type'])
         my_df['Price/Hr'].append(l['Price/Hr'])
-        my_df['Price'].append(float(l['Price/Hr'][1:])*24*7*365)
-        #print(d)
-
+        my_df['Price'].append(float(l['Price/Hr'][1:]) * 24 * 7 * 365)
+        # print(d)
 
     my_df = pd.DataFrame(my_df)
 
     result = pd.concat([df1, my_df], axis=1, sort=False)
 
-    rs = Estimate(result)
-    #print(type(rs))
+    group = result.groupby('Group')
+
+
+    rs = group['Price'].agg([np.sum])
+
+    # print(type(rs))
     costList = []
     for index, row in rs.iterrows():
 
         if index == 'Engineering':
-            year1 = row["sum"]
-            year2 = year1*0.10+year1
-            year3 = year2*0.10+year2
-            costList.append((index, [round(year1, 2), round(year2, 2), round(year3, 2)]))
+            year1 = '${:,.2f}'.format(row["sum"])
+            year2 = row["sum"] * 0.10 + row["sum"]
+            year3 = year2 * 0.10 + year2
+            year2 = '${:,.2f}'.format(year2)
+            year3 = '${:,.2f}'.format(year3)
+            costList.append({'Department': index, 'Year1': year1, 'Year2': year2, 'Year3': year3})
+            #costList.append((index, [round(year1, 2), round(year2, 2), round(year3, 2)]))
 
         if index == 'Sales':
-            year1 = row["sum"]
-            year2 = year1-year1*0.80
-            year3 = year2-year2
-            costList.append((index, [round(year1, 2), round(year2, 2), round(year3, 2)]))
+            year1 = '${:,.2f}'.format(row["sum"])
+            year2 = row["sum"] - row["sum"] * 0.80
+            year3 = year2 - year2
+            year2 = '${:,.2f}'.format(year2)
+            year3 = '${:,.2f}'.format(year3)
+            costList.append({'Department': index, 'Year1': year1, 'Year2': year2, 'Year3': year3})
+            #costList.append((index, [round(year1, 2), round(year2, 2), round(year3, 2)]))
 
         if index == 'Engineering Canada':
-            costList.append((index, [round(row["sum"], 2), round(row["sum"], 2), round(row["sum"], 2)]))
+            year = '${:,.2f}'.format(row["sum"])
+            costList.append({'Department': index, 'Year1': year, 'Year2': year, 'Year3': year})
+            #costList.append((index, [round(row["sum"], 2), round(row["sum"], 2), round(row["sum"], 2)]))
 
         if index == 'Marketing':
-            costList.append((index, [round(row["sum"], 2), round(row["sum"], 2), round(row["sum"], 2)]))
+            year = '${:,.2f}'.format(row["sum"])
+            costList.append({'Department': index, 'Year1': year, 'Year2': year, 'Year3': year})
+            #costList.append((index, [round(row["sum"], 2), round(row["sum"], 2), round(row["sum"], 2)]))
 
-    print(costList)
+    return costList
 
-
-if __name__ == "__main__":
-    main()
