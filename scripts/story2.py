4ee51382c8a673d0b40129553263d6444421fe65
Ishan Desai <idesai1210@gmail.com>
2018-08-07 22:31:44 -0500

    Adding logging to python scripts

--- scripts/story2.py
+++ scripts/story2.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
+import logging
 
-
+logging.basicConfig(filename='Story2.log', format='%(levelname)s:%(asctime)s:%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
 def Estimate(original_df, prices_df):
     # Load a sheet into a DataFrame by name: df_hardware
     df_hardware = original_df
@@ -28,46 +29,54 @@ def Estimate(original_df, prices_df):
 
     pricesDict = pd.DataFrame(pricesDict)
 
-    # Join Dataframes on the primary key index
-    result = pd.concat([df_hardware, pricesDict], axis=1, sort=False)
+    try:
+        # Join Dataframes on the primary key index
+        result = pd.concat([df_hardware, pricesDict], axis=1, sort=False)
 
-    # Group By Department
-    groupByDept = result.groupby('Group')
+        # Group By Department
+        groupByDept = result.groupby('Group')
 
-    # Aggregate to calculate the sum
-    rs = groupByDept['Price'].agg([np.sum])
-
+        # Aggregate to calculate the sum
+        rs = groupByDept['Price'].agg([np.sum])
+        logging.info("Concatenation Successful")
+    except Exception as e:
+        logging.error("Error in concatenating two dataframes")
+        logging.error(repr(e))
     # print(type(rs))
     # Create JSON String to feed to web app
     costList = []
-    for index, row in rs.iterrows():
+    try:
+        for index, row in rs.iterrows():
 
-        if index == 'Engineering':
-            year1 = '${:,.2f}'.format(row["sum"])
-            year2 = row["sum"] * 0.10 + row["sum"]
-            year3 = year2 * 0.10 + year2
-            year2 = '${:,.2f}'.format(year2)
-            year3 = '${:,.2f}'.format(year3)
-            costList.append({'Department': index, 'Year1': year1, 'Year2': year2, 'Year3': year3})
-            # costList.append((index, [round(year1, 2), round(year2, 2), round(year3, 2)]))
+            if index == 'Engineering':
+                year1 = '${:,.2f}'.format(row["sum"])
+                year2 = row["sum"] * 0.10 + row["sum"]
+                year3 = year2 * 0.10 + year2
+                year2 = '${:,.2f}'.format(year2)
+                year3 = '${:,.2f}'.format(year3)
+                costList.append({'Department': index, 'Year1': year1, 'Year2': year2, 'Year3': year3})
+                # costList.append((index, [round(year1, 2), round(year2, 2), round(year3, 2)]))
 
-        if index == 'Sales':
-            year1 = '${:,.2f}'.format(row["sum"])
-            year2 = row["sum"] - row["sum"] * 0.80
-            year3 = year2 - year2
-            year2 = '${:,.2f}'.format(year2)
-            year3 = '${:,.2f}'.format(year3)
-            costList.append({'Department': index, 'Year1': year1, 'Year2': year2, 'Year3': year3})
-            # costList.append((index, [round(year1, 2), round(year2, 2), round(year3, 2)]))
+            if index == 'Sales':
+                year1 = '${:,.2f}'.format(row["sum"])
+                year2 = row["sum"] - row["sum"] * 0.80
+                year3 = year2 - year2
+                year2 = '${:,.2f}'.format(year2)
+                year3 = '${:,.2f}'.format(year3)
+                costList.append({'Department': index, 'Year1': year1, 'Year2': year2, 'Year3': year3})
+                # costList.append((index, [round(year1, 2), round(year2, 2), round(year3, 2)]))
 
-        if index == 'Engineering Canada':
-            year = '${:,.2f}'.format(row["sum"])
-            costList.append({'Department': index, 'Year1': year, 'Year2': year, 'Year3': year})
-            # costList.append((index, [round(row["sum"], 2), round(row["sum"], 2), round(row["sum"], 2)]))
+            if index == 'Engineering Canada':
+                year = '${:,.2f}'.format(row["sum"])
+                costList.append({'Department': index, 'Year1': year, 'Year2': year, 'Year3': year})
+                # costList.append((index, [round(row["sum"], 2), round(row["sum"], 2), round(row["sum"], 2)]))
 
-        if index == 'Marketing':
-            year = '${:,.2f}'.format(row["sum"])
-            costList.append({'Department': index, 'Year1': year, 'Year2': year, 'Year3': year})
-            # costList.append((index, [round(row["sum"], 2), round(row["sum"], 2), round(row["sum"], 2)]))
+            if index == 'Marketing':
+                year = '${:,.2f}'.format(row["sum"])
+                costList.append({'Department': index, 'Year1': year, 'Year2': year, 'Year3': year})
+                # costList.append((index, [round(row["sum"], 2), round(row["sum"], 2), round(row["sum"], 2)]))
+    except Exception as e:
+        logging.error("The result set is empty")
+        logging.error(repr(e))
 
     return costList
